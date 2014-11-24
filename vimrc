ae47621fc81686d6c9b6bc017f9b03c1015245ab
Claus Vium <clausvium@gmail.com>
2014-11-24 12:42:28 +0100

    added files

---
+++ vimrc
@@ -0,0 +1,16 @@
+set nocompatible
+filetype plugin indent on
+set t_Co=256
+syntax enable
+colorscheme monokai 
+set number
+filetype indent on
+set nohlsearch
+set incsearch
+set ruler
+set autoindent
+set tabstop=3
+set softtabstop=3
+set shiftwidth=3
+set shiftround
+set expandtab
---
+++ xmonad/xmobar.hs
@@ -0,0 +1,27 @@
+-- xmobar config used by Cedric Fung on 15" rMBP
+-- Origin: Vic Fryzel (http://github.com/vicfryzel/xmonad-config)
+-- Modifier: Cedric Fung (http://github.com/vec.io/.linux/xmonad)
+
+Config {
+    font = "xft:DejaVu Sans Mono:size=10:antialias=true",
+    bgColor = "#000000",
+    fgColor = "#ffffff",
+    position = Static { xpos = 0, ypos = 0, width = 2560, height = 32 },
+    lowerOnStart = False,
+    allDesktops = True,
+    persistent = True,
+    commands = [
+        -- Run MultiCpu ["-t","CPU: <total0> <total1> <total2> <total3> <total4> <total5> <total6> <total7>","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run CoreTemp ["-t", "Temps: <core0> <core1> <core2>", "-L", "40", "-H", "60", "-l", "lightblue", "-n", "gray90", "-h", "red"] 50,
+        Run Memory ["-t","M: <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run Battery ["-t", "<left>% <timeleft>", "-L", "20", "-H", "75", "-h", "green", "-n", "yellow", "-l", "red"] 60,
+        -- Run DiskIO [("/", "R:<total>"), ("/home", "H:<total>")] [] 10,
+        Run DynNetwork [] 10,
+        Run Date "%a %b %_d %H:%M" "date" 10,
+        Run Com "/home/claus/bin/getvolume.sh" [] "myVolume" 10,
+        Run StdinReader
+    ],
+    sepChar = "%",
+    alignSep = "}{",
+    template = "%StdinReader% }{ %dynnetwork%  %coretemp%  %battery% vol: %myVolume% <fc=#FFFFCC>%date%</fc>"
+}
